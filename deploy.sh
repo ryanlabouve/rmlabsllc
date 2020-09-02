e0a68e1070379bfb88a18f6f62f7203e4b0ac50b
Ryan LaBouve <imryanlabouve@gmail.com>
2020-09-01 20:01:02 -0500

    Adds a deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git branch -D gh-pages
+git co -b gh-pages
+npm run prod
+JEKYLL_ENV=production bundle exec jekyll build
+rm -rf $(ls | sort -u | grep -Ev '_site|deploy.sh')
+mv _site/* .
+git add .
+git commit -m "Deploy"
+git push origin gh-pages -f
+git co master
