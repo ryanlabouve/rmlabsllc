3b9060e4d179dae07c5fbc71e43bf07cdf6f9638
Ryan LaBouve <imryanlabouve@gmail.com>
2020-09-01 20:08:47 -0500

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ git branch -D gh-pages
 git co -b gh-pages
 npm run prod
 JEKYLL_ENV=production bundle exec jekyll build
+rm -rf ./dist
 rm -rf $(ls | sort -u | grep -Ev '_site|deploy.sh')
 mv _site/* .
 git add .
